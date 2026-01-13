ef581659fe15ff85ff7bbbdb06bdeebf3965ffda
SimeonC2026 <simeon.chew2026@gbwl.org>
2026-01-13 16:15:29 +0000

    Unit 3 Activity

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // CREATE 2 Pet objects with different initial values
+      // TEST all Pet methods (getters, toString, setters)
+      Pet p = new Pet(Sophie);
+      p.print(Sophie);
+
    }
 }
---
+++ Pet.java
@@ -0,0 +1,51 @@
+public class Pet {
+    
+    // 1. Declare INSTANCE VARIABLES
+    private String name;
+    private int age;
+    private double weight;
+    private String type;
+    private boolean sterile;
+    
+    
+    // 2. Define CONSTRUCTOR
+public Pet (String initName, int initAge, double initWeight, String initType, boolean initSterile)
+{
+    name = "Sophie";
+    age = 5;
+    weight = 20;
+    type = "dog";
+    sterile = false;
+}
+    // 3. Define METHODS
+    // getters
+    public String getName () {
+        return name;
+    }
+    public int getAge () {
+        return age;
+    }
+    public double getWeight () {
+        return weight;
+    }
+    public String getType () {
+        return type;
+    }
+    public boolean getSterile () {
+        return sterile;
+    }
+     
+    // toString
+    public String toString() {
+        return age + ": " + name + ", " + type;
+    }
+    // setters
+    public void setName (String newName) {
+        this.name = newName;
+    }
+    public void setAge (int newAge) {
+        this.age = newAge;
+    }
+
+
+}
